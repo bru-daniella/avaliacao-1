570d63e68c1c05ac38ba02f431c4b7de39ab1913
Bruna Daniella <78834625+bru-daniella@users.noreply.github.com>
2025-10-10 20:19:49 -0300

    Cria portfolio

---
+++ main.js
@@ -0,0 +1,21 @@
+//mudar icone principal
+
+
+// function trocar(){
+//     
+// }
+
+function trocar() {
+    const elementoicone = document.getElementById("icone");
+    elementoicone.id = "icone-2-js";
+
+    const toponav = document.getElementsByClassName("navigator");
+    toponav[0].style.backgroundColor = "white"; 
+  }
+  
+
+
+//mudar banner principal
+
+
+//mudar cor do topo
